5afe53a41d1dd18c939e967a5f63c13c50a2129a
Louis Boeckmans <louis.boeckmans@outlook.com>
2023-12-13 13:53:12 +0100

    main

--- src/app/modules/designcheats/accordions/accordion-1/accordion-1.component.ts
+++ src/app/modules/designcheats/accordions/accordion-1/accordion-1.component.ts
@@ -73,18 +73,117 @@ export class Accordion1Component {
       
   }
 
-  getAccordionContentClass(index: number) : void
+  getAccordionContentClass(index: number) : string | undefined
   {
+    let accordion = this.accordions.Accordions[index];
+    
+    if(index === this.accordions.Accordions.length - 1)
+    {
+      //dernier content class
+      if(accordion.isExpanded && !accordion.isExpanding)
+      {
+        return 'accordion-content-last';
+      }
+
+      if(accordion.isExpanded && accordion.isExpanding)
+      {
+        return 'accordion-content-last-expanding';
+      }
+
+      if(!accordion.isExpanded && accordion.isContracting)
+      {
+        return 'accordion-content-last-contracting';
+      }
+    }
+    else
+    {
+      if(accordion.isExpanded && !accordion.isExpanding)
+      {
+        return 'accordion-content';
+      }
+
+      if(accordion.isExpanded && accordion.isExpanding)
+      {
+        return 'accordion-content-expanding';
+      }
+
+      if(!accordion.isExpanded && accordion.isContracting)
+      {
+        return 'accordion-content-contracting';
+      }
+    }
+
+    return undefined
 
   }
 
-  getAccordionHeaderClass(index: number) : void
+  getAccordionHeaderClass(index: number) : string | undefined
   {
+    let accordion = this.accordions.Accordions[index];
+
+    if(index === this.accordions.Accordions.length - 1)
+    {
+      if(accordion.isExpanded)
+      {
+        return 'accordion-last-element-header'
+      }
+      else
+      {
+        return 'accordion-last-element-header-contracted'
+      }
+    }
+    else
+    {
+      if(index === 0)
+      {
+        //first header
+        if(accordion.isExpanded)
+        {
+          return 'accordion-first-element-header'
+        }
+        else
+        {
+          return 'accordion-first-element-header-contracted'
+        }
+      }
+      else
+      {
+        if(accordion.isExpanded)
+        {
+          return 'accordion-element-header'
+        }
+        else
+        {
+          return 'accordion-element-header-contracted'
+        }
+      }
+    }
+
+    return undefined
 
   }
 
-  getExpanderClass(index: number) : void
+  getExpanderClass(index: number) : string
   {
+    let accordion = this.accordions.Accordions[index];
+
+    if(accordion.isExpanded && !accordion.isExpanding)
+    {
+      return 'accordion-expander';
+    }
+
+    if(accordion.isExpanded && accordion.isExpanding)
+    {
+      return 'accordion-expander-expanding';
+    }
+
+    if(!accordion.isExpanded && accordion.isContracting)
+    {
+      return 'accordion-expander-contracting';
+    }
+
+    return 'accordion-expander'
+
     
   }
 
